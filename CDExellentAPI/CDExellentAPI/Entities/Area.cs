19ed68632eafcf3bd70dd5d4fde577ff475ff7f4
hoainam3152 <hoainam3152@gmail.com>
2024-12-11 17:55:12 +0700

    init project

---
+++ CDExellentAPI/CDExellentAPI/Entities/Area.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CDExellentAPI.Entities
+{
+    [Table("Area")]
+    public class Area
+    {
+        [Key]
+        [StringLength(100)]
+        public string ID { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+    }
+}
---
+++ CDExellentAPI/CDExellentAPI/Entities/ManagementDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CDExellentAPI.Entities
+{
+    public class ManagementDbContext : DbContext
+    {
+        public ManagementDbContext(DbContextOptions options) : base(options) { }
+
+        #region DbSet
+        public DbSet<Area> Areas { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
